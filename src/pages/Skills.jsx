ae43f11ec36d1a5e23b9649f2bf8274c0c326f07
shyam1998 <shyamsundarsahoo1998@gmail.com>
2024-09-15 22:30:18 +0530

    skills css and image update

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -8,10 +8,13 @@ import routerimage from "../../public/images/react router.png";
 import nodejsimage from "../../public/images/node js image.png";
 import expressjsimage from "../../public/images/express js image.jpg";
 import sqlimage from "../../public/images/sql image.png";
-import postgressqimage  from "../../public/images/postgressql image.png";
+import postgressqimage from "../../public/images/postgressql image.png";
 import githubimage from "../../public/images/github image.png";
 import nginximage from "../../public/images/nginx image.png";
 import pm2image from "../../public/images/pm2 image.png";
+import postmanimage from "../../public/images/postman image.png";
+import vscodeimage from "../../public/images/vs code image.jpg";
+import upcommingimage from "../../public/images/upcoming image.png";
 
 function Skills({ setProgress }) {
   useEffect(() => {
@@ -26,58 +29,75 @@ function Skills({ setProgress }) {
     <>
       {/*Skills Section*/}
       <section className="skills-section">
-        <div className="container grid grid-three-cols">
-          <div className="skills-card">
-            <img src={htmlimage} alt="image not available" />
-            <h2>Html</h2>
+        <div className="container ">
+          <div className="common-title-container">
+            <h2 className="title">My Skills</h2>
           </div>
-          <div className="skills-card">
-            <img src={cssimage} alt="image not available" />
-            <h2>Css</h2>
-          </div>
-          <div className="skills-card">
-            <img src={jsimage} alt="image not available" />
-            <h2>Java Script</h2>
-          </div>
-          <div className="skills-card">
-            <img src={reactimage} alt="image not available" />
-            <h2>React</h2>
-          </div>
-          <div className="skills-card">
-            <img src={reduximage} alt="image not available" />
-            <h2>Redux</h2>
-          </div>
-          <div className="skills-card">
-            <img src={routerimage} alt="image not available" />
-            <h2>React Router</h2>
-          </div>
-          <div className="skills-card">
-            <img src={nodejsimage} alt="image not available" />
-            <h2>Node js</h2>
-          </div>
-          <div className="skills-card">
-            <img src={expressjsimage} alt="image not available" />
-            <h2>Express js</h2>
-          </div>
-          <div className="skills-card">
-            <img src={sqlimage} alt="image not available" />
-            <h2>Sql</h2>
-          </div>
-          <div className="skills-card">
-            <img src={postgressqimage} alt="image not available" />
-            <h2>Postgre Sql</h2>
-          </div>
-          <div className="skills-card">
-            <img src={githubimage} alt="image not available" />
-            <h2>Git Hub</h2>
-          </div>
-          <div className="skills-card">
-            <img src={nginximage} alt="image not available" />
-            <h2>Nginx</h2>
-          </div>
-          <div className="skills-card">
-            <img src={pm2image} alt="image not available" />
-            <h2>Pm2</h2>
+          <div className="cards grid grid-three-cols">
+            <div className="skills-card">
+              <img src={htmlimage} alt="image not available" />
+              <h2>Html</h2>
+            </div>
+            <div className="skills-card">
+              <img src={cssimage} alt="image not available" />
+              <h2>Css</h2>
+            </div>
+            <div className="skills-card">
+              <img src={jsimage} alt="image not available" />
+              <h2>Java Script</h2>
+            </div>
+            <div className="skills-card">
+              <img src={reactimage} alt="image not available" />
+              <h2>React</h2>
+            </div>
+            <div className="skills-card">
+              <img src={reduximage} alt="image not available" />
+              <h2>Redux</h2>
+            </div>
+            <div className="skills-card">
+              <img src={routerimage} alt="image not available" />
+              <h2>React Router</h2>
+            </div>
+            <div className="skills-card">
+              <img src={nodejsimage} alt="image not available" />
+              <h2>Node js</h2>
+            </div>
+            <div className="skills-card">
+              <img src={expressjsimage} alt="image not available" />
+              <h2>Express js</h2>
+            </div>
+            <div className="skills-card">
+              <img src={sqlimage} alt="image not available" />
+              <h2>Sql</h2>
+            </div>
+            <div className="skills-card">
+              <img src={postgressqimage} alt="image not available" />
+              <h2>Postgre Sql</h2>
+            </div>
+            <div className="skills-card">
+              <img src={githubimage} alt="image not available" />
+              <h2>Git Hub</h2>
+            </div>
+            <div className="skills-card">
+              <img src={nginximage} alt="image not available" />
+              <h2>Nginx</h2>
+            </div>
+            <div className="skills-card">
+              <img src={pm2image} alt="image not available" />
+              <h2>Pm2</h2>
+            </div>
+            <div className="skills-card">
+              <img src={vscodeimage} alt="image not available" />
+              <h2>VS Code</h2>
+            </div>
+            <div className="skills-card">
+              <img src={postmanimage} alt="image not available" />
+              <h2>Postman</h2>
+            </div>
+            <div className="skills-card">
+              <img src={upcommingimage} alt="image not available" />
+              <h2>Upcommings</h2>
+            </div>
           </div>
         </div>
       </section>
