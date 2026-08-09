66b038ffa9221c7d518ecd6c59d929218ffff012
shyam01999 <shyamsundarsahoo1998@gmail.com>
2026-08-09 22:32:37 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio">
         <LoadingBar
           color={themecolor}
           progress={progress}
